a27f338810e7a1a21c753e1058adc1e83b5948bf
Jakub Kuchta <jakub.kuchta@rockwellautomation.com>
2023-02-06 15:37:33 +0100

    Working version for ESP32

--- CareLight/CareLight.ino
+++ CareLight/CareLight.ino
@@ -1,176 +1,210 @@
-/**
-   BasicHTTPSClient.ino
-
-    Created on: 20.08.2018
-
-*/
-
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiMulti.h>
+#include <HTTPClient.h>
+#include <WiFiClientSecure.h>
 
-#include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
-
-#include <ESP8266HTTPClient.h>
-
-#include <WiFiClientSecureBearSSL.h>
-// Fingerprint for demo URL, expires on June 2, 2021, needs to be updated well before this date
-//const uint8_t fingerprint[20] = { 0x40, 0xaf, 0x00, 0x6b, 0xec, 0x90, 0x22, 0x41, 0x8e, 0xa3, 0xad, 0xfa, 0x1a, 0xe8, 0x25, 0x41, 0x1d, 0x1a, 0x54, 0xb3 };
-
-//#define DEBUG_ESP_HTTP_CLIENT
-//#define DEBUG_ESP_PORT Serial
-
-ESP8266WiFiMulti WiFiMulti;
+WiFiMulti WiFiMulti;
 String payload;
-
+CookieJar cookies;
 
 void setup() {
-  payload.reserve(2500);  
+  //payload.reserve(2500);  
   Serial.begin(115200);
-  // Serial.setDebugOutput(true);
+  Serial.println();
+  Serial.println();
+  Serial.println();
+  Serial.println("Starting...");
+  //Serial.flush();
+  //delay(3000);
+  
+  //WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP("baku2", "12837455");
 
-  Serial.println();
-  Serial.println();
-  Serial.println();
-
-  for (uint8_t t = 4; t > 0; t--) {
-    Serial.printf("[SETUP] WAIT %d...\n", t);
-    Serial.flush();
+  Serial.print("Waiting for WiFi to connect...");
+  while ((WiFiMulti.run() != WL_CONNECTED)) {
+    Serial.print(".");
     delay(1000);
   }
-
-  WiFi.mode(WIFI_STA);
-  WiFiMulti.addAP("baku", "12837455");
+  Serial.println(" connected");
 }
 
 String exractParam(String authReq, const String& param, const char delimit) {
   int _begin = authReq.indexOf(param);
-  Serial.print(param);
   if (_begin == -1) 
-  { 
-    Serial.println("Not found");    
+  {    
     return ""; 
   }
-  Serial.println("Found");
   String result = authReq.substring(_begin + param.length(), authReq.indexOf(delimit, _begin + param.length()));
   return result;
 }
 
 void loop() {
-  // wait for WiFi connection
-  if ((WiFiMulti.run() == WL_CONNECTED)) {
-
-    std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
+  WiFiClientSecure *client = new WiFiClientSecure;
+  
+  if (client) {
     client->setInsecure();
+    {
+      HTTPClient https;
+      https.setCookieJar(&cookies);
 
-    HTTPClient https;
+      Serial.println(F("Get login page:"));
+      if (https.begin(*client, F("https://carelink.minimed.eu/patient/sso/login?country=pl&lang=en"))) {
+        https.setFollowRedirects(HTTPC_FORCE_FOLLOW_REDIRECTS);
+        https.setTimeout(60000);
+        https.setReuse(true);
 
-    Serial.println(F("Get login page:"));
-    if (https.begin(*client, F("https://carelink.minimed.eu/patient/sso/login?country=pl&lang=en"))) {
-      https.setFollowRedirects(HTTPC_FORCE_FOLLOW_REDIRECTS);
-      //https.setTimeout(60000);
-      https.setReuse(true);
-      Serial.print("[HTTPS] GET");
-      int httpCode = https.GET();
-      
-      if (httpCode > 0) {
-        // HTTP header has been send and Server response header has been handled
-        Serial.print(F("[HTTPS] Response code: ")); 
+        int httpCode = https.GET();
         Serial.println(httpCode);
         
-        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-          https.getString();
-          //Serial.println(payload.c_str());
+        if (httpCode > 0) {
           
-          const String& location = https.getLocation();
-          Serial.println(F("Extract sessionID and sessionData:"));
-          Serial.println(location.c_str());
-          String sessionId = exractParam(location.c_str(), F("sessionID="), '&');
-          String sessionData = exractParam(location.c_str(), F("sessionData="), '&');
-          Serial.printf("SessionID:   %s\n", sessionId.c_str());
-          Serial.printf("SessionData: %s\n\n", sessionData.c_str());       
-          https.end();
-
-          Serial.println("Submit login:");
-          //if (https.begin(*client, "https://www.wp.pl")) {          
-          if (https.begin(*client, F("https://mdtlogin.medtronic.com/mmcl/auth/oauth/v2/authorize/login?locale=en&country=pl"))) {
-            https.setFollowRedirects(HTTPC_FORCE_FOLLOW_REDIRECTS);
-            https.setTimeout(60000);
-            https.addHeader(F("Accept"), F("text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"));
-            https.addHeader(F("Accept-Language"), F("en-US,en;q=0.9"));
-            //https.addHeader("Accept-Encoding", "identity");
-            //https.addHeader("sec-ch-ua", "\"Google Chrome\";v=\"87\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"87\"");
-            https.addHeader(F("Content-Type"), F("application/x-www-form-urlencoded"));
-            https.setReuse(true);/////////////////////////////////////
-            https.setUserAgent(F("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.70"));                    
-            String content = String("sessionID=" + sessionId + "&sessionData=" + sessionData + "&locale=en&action=login&username=sylwiadk&password=Mamba2018&actionButton=Log+in");
-            Serial.printf("[HTTPS] POST: %s\n", content.c_str());
-            httpCode = https.POST(content);
-            Serial.printf("[HTTPS] Response code: %d\n", httpCode);
-            if (httpCode > 0) {
-              Serial.printf("[HTTPS] Response code: %d\n", httpCode);             
-
-              // file found at server
-              if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) 
-              {
-                payload = https.getString();
-                Serial.println(payload.c_str());
-
-                String consentUrl         = exractParam(payload.c_str(), "form action=\"", '"');
-                String consentSessionId   = exractParam(payload.c_str(), "\"sessionID\" value=\"", '"');
-                String consentSessionData = exractParam(payload.c_str(), "\"sessionData\" value=\"", '"');                
-                Serial.printf("consentUrl:         %s\n", consentUrl.c_str());
-                Serial.printf("consentSessionData: %s\n", consentSessionData.c_str());
-                Serial.printf("ConsentSessionId:   %s\n", consentSessionId.c_str());
-                Serial.printf("SessionID:          %s\n", sessionId.c_str());                
-                https.end();
-
-                Serial.println("Submit consent:");
-                if (https.begin(*client, consentUrl.c_str())) {
-                  https.setFollowRedirects(HTTPC_FORCE_FOLLOW_REDIRECTS);
-                  https.setTimeout(60000);
-                  https.addHeader(F("Accept"), F("text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"));
-                  https.addHeader(F("Accept-Language"), F("en-US,en;q=0.9"));
-                  https.addHeader(F("Content-Type"), F("application/x-www-form-urlencoded"));
-                  https.setReuse(true);
-                  //https.setUserAgent(F("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0"));                    
-                  content = String("action=consent&sessionID=" + consentSessionId + "&sessionData=" + consentSessionData + "&response_type=code&response_mode=query");
-                  Serial.printf("[HTTPS] POST: %s\n", content.c_str());
-                  httpCode = https.POST(content.c_str());
-
-                  if (httpCode > 0) {
-                    Serial.printf("[HTTPS] Response code: %d\n", httpCode);             
-
-                    if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) 
-                    {
-                      https.getString();
-                      //Serial.println(payload.c_str());
-
-                      Serial.println("All cookies:");
-                      Serial.println(https.cookies());
-                      for(int j=0; j<https.cookies(); j++){
-                        Serial.println(https.cookie(j));
-                      } 
-                      https.end();                      
-                    }
-                  }
-                }                  
-              }
-            }  
-            else
-            {
-              Serial.printf("[HTTPS] POST failed... failed, error: %s\n", https.errorToString(httpCode).c_str());
-            }                               
-          }
+          if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+            https.getString();
+            //Serial.println(payload.c_str());
             
-          
-        }
-      } else {
-        Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
-      }
+            const String& location = https.getLocation();
+            //Serial.println(F("Extract sessionID and sessionData:"));
+            //Serial.println(location.c_str());
+            String sessionId = exractParam(location.c_str(), F("sessionID="), '&');
+            String sessionData = exractParam(location.c_str(), F("sessionData="), '&');
+            Serial.printf("SessionID:   %s\n", sessionId.c_str());
+            Serial.printf("SessionData: %s\n\n", sessionData.c_str());       
+            https.end();
 
-      https.end();
-    } else {
-      Serial.printf("[HTTPS] Unable to connect\n");
+            Serial.println("Submit login:");        
+            if (https.begin(*client, F("https://mdtlogin.medtronic.com/mmcl/auth/oauth/v2/authorize/login?locale=en&country=pl"))) {
+              https.setFollowRedirects(HTTPC_FORCE_FOLLOW_REDIRECTS);
+              https.setTimeout(60000);
+              https.addHeader(F("Accept"), F("text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"));
+              //https.addHeader(F("Accept-Encoding"), F("gzip, deflate, br"));
+              //https.addHeader(F("Accept-Language"), F("en-US,en;q=0.9"));
+              //https.addHeader(F("Cache-Control"), F("max-age=0"));
+              https.setReuse(true);
+              https.addHeader(F("Content-Type"), F("application/x-www-form-urlencoded"));
+              //https.addHeader(F("sec-ch-ua"), F("\"Not_A Brand\";v=\"99\", \"Microsoft Edge\";v=\"109\", \"Chromium\";v=\"109\""));
+              //https.addHeader(F("sec-ch-ua-mobile"), F("?0"));
+              //https.addHeader(F("sec-ch-ua-platform"), F("\"Windows\""));
+              //https.addHeader(F("Sec-Fetch-Dest"), F("document"));
+              //https.addHeader(F("Sec-Fetch-Mode"), F("navigate"));
+              //https.addHeader(F("Sec-Fetch-Site"), F("same-origin"));
+              //https.addHeader(F("Upgrade-Insecure-Requests"), F("1"));
+              //https.setUserAgent(F("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.70"));                    
+              
+              String content = String("sessionID=" + sessionId + "&sessionData=" + sessionData + "&locale=en&action=login&username=sylwiadk&password=Mamba2018&actionButton=Log+in");
+              Serial.printf("Content: %s\n", content.c_str());
+              
+              httpCode = https.POST(content);
+              Serial.println(httpCode);
+              
+              if (httpCode > 0) {
+
+                if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) 
+                {
+                  payload = https.getString();
+                  //Serial.println(payload.c_str());
+
+                  String consentUrl         = exractParam(payload.c_str(), "form action=\"", '"');
+                  String consentSessionId   = exractParam(payload.c_str(), "\"sessionID\" value=\"", '"');
+                  String consentSessionData = exractParam(payload.c_str(), "\"sessionData\" value=\"", '"');                
+                  Serial.printf("ConsentUrl:         %s\n", consentUrl.c_str());
+                  Serial.printf("ConsentSessionData: %s\n", consentSessionData.c_str());
+                  Serial.printf("ConsentSessionId:   %s\n\n", consentSessionId.c_str());
+
+                  Serial.println("All cookies:");
+                  Serial.println(cookies.size());
+
+          
+                  https.end();
+
+                  Serial.println("Submit consent:");
+                  if (https.begin(*client, consentUrl.c_str())) {
+                    https.setFollowRedirects(HTTPC_FORCE_FOLLOW_REDIRECTS);
+                    https.setTimeout(60000);
+
+                    https.setUserAgent(F("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0"));
+                    https.addHeader(F("Accept"), F("text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8"));
+                    https.addHeader(F("Accept-Language"), F("en-US,en;q=0.9"));
+                    https.addHeader(F("Content-Type"), F("application/x-www-form-urlencoded"));
+                    https.addHeader(F("Origin"), F("https://mdtlogin.medtronic.com"));
+                    https.setReuse(true);
+                    https.addHeader(F("Referer"), F("https://mdtlogin.medtronic.com/mmcl/auth/oauth/v2/authorize/login?locale=en&country=pl"));
+                    
+                    content = String("action=consent&sessionID=" + consentSessionId + "&sessionData=" + consentSessionData + "&response_type=code&response_mode=query");
+                    Serial.printf("Content: %s\n", content.c_str());
+                    httpCode = https.POST(content.c_str());
+                    Serial.println(httpCode);
+
+                    if (httpCode > 0) {             
+
+                      if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) 
+                      {
+                        https.getString();
+                        String authToken;
+                        for (int i = 0; i < cookies.size(); i++){
+                          if(cookies[i].name.equals("auth_tmp_token")){
+                            authToken = cookies[i].value;
+                            Serial.println("Auth cookie:");
+                            Serial.println(authToken.c_str());
+                          }
+                        }
+                        https.end();  
+
+
+                        Serial.println("Get data:");
+                        if (https.begin(*client, "https://clcloud.minimed.eu/connect/v2/display/message")) {
+                          https.setFollowRedirects(HTTPC_FORCE_FOLLOW_REDIRECTS);
+                          https.setTimeout(60000);
+
+                          https.setUserAgent(F("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0"));
+                          https.addHeader(F("Accept"), F("text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8"));
+                          https.addHeader(F("Accept-Language"), F("en-US,en;q=0.9"));
+                          https.addHeader(F("Content-Type"), F("application/json; charset=utf-8"));
+                          https.setReuse(true);
+                          https.setAuthorizationType("Bearer");
+                          https.setAuthorization(authToken.c_str());
+                          content = "{\"username\":\"sylwiadk\",\"role\":\"patient\"}";
+                          Serial.printf("Content: %s\n", content.c_str());
+                          httpCode = https.POST(content.c_str());
+                          Serial.println(httpCode);
+
+                          if (httpCode > 0) {             
+
+                            if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) 
+                            {
+                              payload = https.getString();
+                              String lastTrend = exractParam(payload.c_str(), "\"lastSGTrend\":\"", '"');
+                              String lastSg = exractParam(payload.c_str(), "\"lastSG\":{\"sg\":", ','); 
+
+                              Serial.print("Trend: ");
+                              Serial.println(lastTrend.c_str());
+                              Serial.print("SG: ");
+                              Serial.println(lastSg.c_str());
+                              https.end();                   
+                            }
+                          }
+                        }                    
+                      }
+                    }
+                  }                  
+                }
+              }  
+              else
+              {
+                Serial.printf("[HTTPS] POST failed... failed, error: %s\n", https.errorToString(httpCode).c_str());
+              }                               
+            }
+              
+            
+          }
+        } else {
+          Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+        }
+
+        https.end();
+      } else {
+        Serial.printf("[HTTPS] Unable to connect\n");
+      }
     }
   }
 
