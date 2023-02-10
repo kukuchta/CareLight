5b8f824ef5b70276543702e67263c3321cf91de3
Jakub Kuchta <jakub.kuchta@rockwellautomation.com>
2023-02-10 16:41:57 +0100

    Added Fastled 8x8 led module, fixed cookie cleaning

--- CareLight/CareLight.ino
+++ CareLight/CareLight.ino
@@ -3,6 +3,11 @@
 #include <HTTPClient.h>
 #include <WiFiClientSecure.h>
 #include <ArduinoJson.h>
+#include <FastLED.h>
+
+#define NUM_LEDS 64
+#define DATA_PIN 13
+#define BRIGHTNESS 60
 
 WiFiClientSecure *client;
 HTTPClient https;
@@ -15,17 +20,35 @@ String consentUrl;
 String consentSessionId;
 String consentSessionData;
 String content;
+byte sg;
+CRGB leds[NUM_LEDS];
+CRGBPalette16 myPal;
+
 int httpCode;
 
 unsigned long previousMillis;
 unsigned long currentMillis;
 unsigned long period = 300000;
 
+DEFINE_GRADIENT_PALETTE( heatmap_gp ) {
+  0,     0,  0,  0,   //<40 black
+  1,   122,  1,254,   //40  violet
+ 23,   255,  0,  0,   //70  red
+ 24,   255,255,  0,   //71  yellow-green
+ 51,     1,251,255,   //110 turquoise
+ 72,     2,222,255,   //140 blue turquoise
+100,   255,255,  0,   //180 yellow
+150,   255,  0,  0,   //250 red
+255,   255,  0,255 }; //400 red
+
 void setup() {
   Serial.begin(115200);
   Serial.println();
   Serial.println();
   Serial.println();
+  myPal = heatmap_gp;
+  FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS).setCorrection( TypicalSMD5050 );
+  FastLED.setBrightness( BRIGHTNESS );
   Serial.println("Starting...");
   https.setCookieJar(&cookies);
 }
@@ -144,6 +167,7 @@ bool SubmitConsent() {
 }
 
 bool Login() {
+  https.clearAllCookies();
   bool success = GetLoginPage();
 
   if (success == true) {
@@ -158,7 +182,7 @@ bool Login() {
 }
 
 bool GetData() {
-  Serial.println("Get data:");
+  Serial.print("Get data: ");
   if (https.begin(*client, "https://clcloud.minimed.eu/connect/v2/display/message")) {
     https.setFollowRedirects(HTTPC_FORCE_FOLLOW_REDIRECTS);
     https.setTimeout(60000);
@@ -180,11 +204,11 @@ bool GetData() {
     if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {             
 ////////////////////////////////////////////////////////////
       WiFiClient * stream = https.getStreamPtr();
-      StaticJsonDocument<64> filter;
+      StaticJsonDocument<256> filter;
       filter["lastSGTrend"] = true;
       filter["lastSG"]["sg"] = true;
 
-      StaticJsonDocument<256> doc;
+      StaticJsonDocument<1024> doc;
 
       DeserializationError error = deserializeJson(doc, *stream, DeserializationOption::Filter(filter));
 
@@ -199,20 +223,49 @@ bool GetData() {
         lastSGTrend = doc["lastSGTrend"]; // "NONE"
         lastSG_sg = doc["lastSG"]["sg"]; // 172        
       }
+/////////////////////////////////////////////////////////////////
+      //Serial.print("[");
+      //int len = https.getSize();
+      //uint8_t buff[512];
+      //// get tcp stream
+      //WiFiClient * stream = https.getStreamPtr();
 
-            
+      //// read all data from server
+      //while(https.connected() && (len > 0 || len == -1)) {
+      //    // get available data size
+      //    size_t size = stream->available();
+
+      //    if(size) {
+      //        // read up to 128 byte
+      //        int c = stream->readBytes(buff, ((size > sizeof(buff)) ? sizeof(buff) : size));
+
+      //        // write it to Serial
+      //        Serial.write(buff, c);
+
+      //        if(len > 0) {
+      //            len -= c;
+      //        }
+      //    }
+      //    delay(1);
+      //}   
+      //Serial.println("] End");       
 
 
 ////////////////////////////////////////////////////////////////////
-      //String lastTrend = exractParam(payload, "\"lastSGTrend\":\"", '"');
-      //String lastSg = exractParam(payload, "\"lastSG\":{\"sg\":", ','); 
+      ////String lastTrend = exractParam(payload, "\"lastSGTrend\":\"", '"');
+      ////String lastSg = exractParam(payload, "\"lastSG\":{\"sg\":", ','); 
 
-      Serial.print("  Trend: ");
-      Serial.println(lastSGTrend);
+      Serial.print("Trend: ");
+      Serial.print(lastSGTrend);
       Serial.print("  SG: ");
-      Serial.println(lastSG_sg);
-
-      
+      Serial.print(lastSG_sg);
+      sg = byte(lastSG_sg * 0.702777 - 26.111111); // Scale to 1-255
+      Serial.print("  Scaled: ");
+      Serial.println(sg);
+      for (int i=0; i<NUM_LEDS; i++) {
+        leds[i] = ColorFromPalette( myPal, sg);
+      }
+      FastLED.show();
       https.end();/////////////
       return true;                
     }
@@ -241,7 +294,7 @@ void connectToWiFi()
 void PeriodicGetData() {
   bool success = GetData();
   
-  if (success != true) {
+  if (success != true) {    
     success = Login();
     if (success == true) {
       success = GetData();
@@ -262,7 +315,6 @@ void loop() {
     connectToWiFi();
     Serial.println("WiFi client connected");
     previousMillis = millis();
-    https.clearAllCookies();
     PeriodicGetData();
   }  
 
